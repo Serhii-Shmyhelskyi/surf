0e2c5ed18754c48a6922a9bda46c1eac4658cbab
ShmyhelskyiSerhii <siro181a@gmail.com>
2020-09-18 16:40:39 +0300

    add header, slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
 $(function () {
 
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class ="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt=""></img>',
+        nextArrow: '<img class ="slider-arrows slider-arrows__right" src="images/arrow-right.svg" alt=""></img>',
+        asNavFor: '.slider-dots',
+    });
 
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    });
 
 
 })
